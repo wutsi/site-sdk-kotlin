6bde7dcbbdf4055c6802473cc0631d3b5c875e96
htchepannou <htchepannou@expedia.com>
2021-04-06 21:44:37 -0400

    fix environment urls

--- src/main/kotlin/com/wutsi/site/Environment.kt
+++ src/main/kotlin/com/wutsi/site/Environment.kt
@@ -1,6 +1,6 @@
 package com.wutsi.site
 
 enum class Environment(public val url: String) {
-  PRODUCTION("https://wutsi-site-test.herokuapp.com"),
+  PRODUCTION("https://wutsi-site-prod.herokuapp.com"),
   SANDBOX("https://wutsi-site-test.herokuapp.com")
 }
