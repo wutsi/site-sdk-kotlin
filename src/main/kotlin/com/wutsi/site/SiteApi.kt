871114d0045a8d1fdc3ae71dc576ef397bd0be26
htchepannou <htchepannou@expedia.com>
2021-04-25 22:41:46 -0400

    update api

--- src/main/kotlin/com/wutsi/site/SiteApi.kt
+++ src/main/kotlin/com/wutsi/site/SiteApi.kt
@@ -3,11 +3,13 @@ package com.wutsi.site
 import com.wutsi.site.dto.CreateSiteRequest
 import com.wutsi.site.dto.CreateSiteResponse
 import com.wutsi.site.dto.GetSiteResponse
+import com.wutsi.site.dto.SearchSiteResponse
 import com.wutsi.site.dto.SetAttributeRequest
 import com.wutsi.site.dto.UpdateSiteRequest
 import com.wutsi.site.dto.UpdateSiteResponse
 import feign.Param
 import feign.RequestLine
+import kotlin.Int
 import kotlin.Long
 import kotlin.String
 import kotlin.Unit
@@ -25,6 +27,10 @@ public interface SiteApi {
     @Param("urn") urn: String,
     request: SetAttributeRequest
   ): Unit
+
+  @RequestLine("GET /v1/sites?limit={limit}&offset={offset}")
+  public fun search(@Param("limit") limit: Int = 20, @Param("offset") offset: Int = 0):
+      SearchSiteResponse
 
   @RequestLine("POST /v1/sites")
   public fun create(request: CreateSiteRequest): CreateSiteResponse
