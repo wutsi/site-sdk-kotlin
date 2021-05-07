e78d8ae788be84529364a29d64d09651363f9197
htchepannou <htchepannou@expedia.com>
2021-05-06 23:09:37 -0400

    add events

---
+++ src/main/kotlin/com/wutsi/site/event/SiteEventPayload.kt
@@ -0,0 +1,5 @@
+package com.wutsi.site.event
+
+data class SiteEventPayload(
+    val siteId: Long
+)
---
+++ src/main/kotlin/com/wutsi/site/event/SiteEventStream.kt
@@ -0,0 +1,5 @@
+package com.wutsi.site.event
+
+object SiteEventStream {
+    val NAME = "site"
+}
